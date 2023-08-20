51da348fa3c34df498d0b46db089622c923e9632
30minutesperday <142711149+30minutesperday@users.noreply.github.com>
2023-08-19 23:12:11 -0700

    sample app using @Extension

---
+++ ManifoldApp/src/main/java/com/mycompany/MainApp.java
@@ -0,0 +1,10 @@
+package com.mycompany;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        String title = "Ms";
+
+        System.out.println("Hello "+title.assignGreeting());
+    }
+}
